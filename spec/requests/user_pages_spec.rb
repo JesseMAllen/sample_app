dfe94f7228283f4f2e5860f5e29d7a60f574639d
Jesse McGil Allen <jmallen7@gmail.com>
2012-11-25 22:19:05 -0600

    exercise 2 completed

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -14,8 +14,9 @@ describe "User pages" do
   
   describe "profile page" do
     let(:user) { FactoryGirl.create(:user) }
-    let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
-    let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
+    
+    before(:all) { 50.times { FactoryGirl.create(:micropost, user: user) } }
+    after(:all)  { Micropost.delete_all }
     
     before { visit user_path(user) }
     
@@ -23,9 +24,18 @@ describe "User pages" do
     it { should have_selector('title', text: user.name) }
     
     describe "microposts" do
-      it { should have_content(m1.content) }
-      it { should have_content(m2.content) }
       it { should have_content(user.microposts.count) }
+      
+      describe "pagination" do
+
+        it { should have_selector('div.pagination') }
+
+        it "should list each user" do
+          Micropost.paginate(page: 1).each do |micropost|
+          page.should have_content(micropost.content)
+          end
+        end
+      end
     end
   end
   
