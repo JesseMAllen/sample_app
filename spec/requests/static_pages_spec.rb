ea7d55b7b9a4705c76922b0800ecd9b7c6ed8d0c
Jesse McGil Allen <jmallen7@gmail.com>
2012-11-28 20:02:40 -0600

    Through 11.2.5

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -28,10 +28,22 @@ describe "Static pages" do
 
       it "should render the user's feed" do
         user.feed.each do |item|
-        page.should have_selector("li##{item.id}", text: item.content)
+          page.should have_selector("li##{item.id}", text: item.content)
+        end
+      end
+
+      describe "follower/following counts" do
+        let(:other_user) { FactoryGirl.create(:user) }
+        before do
+          other_user.follow!(user)
+          visit root_path
+        end
+
+        it { should have_link("0 following", href: following_user_path(user)) }
+        it { should have_link("1 followers", href: followers_user_path(user)) }
+        end
       end
     end
-  end
 
   describe "Help page" do
     before { visit help_path }
@@ -58,18 +70,17 @@ describe "Static pages" do
   end
   
   it "should have the right links on the layout" do
-      visit root_path
-      click_link "About"
-      page.should have_selector 'title', text: full_title('About Us')
-      click_link "Help"
-      page.should have_selector 'title', text: full_title('Help')
-      click_link "Contact"
-      page.should have_selector 'title', text: full_title('Contact')
-      click_link "Home"
-      click_link "Sign up now!"
-      page.should have_selector 'title', text: 'Sign up'
-      click_link "sample app"
-      page.should have_selector 'title', text: full_title('')
-    end
+    visit root_path
+    click_link "About"
+    page.should have_selector 'title', text: full_title('About Us')
+    click_link "Help"
+    page.should have_selector 'title', text: full_title('Help')
+    click_link "Contact"
+    page.should have_selector 'title', text: full_title('Contact')
+    click_link "Home"
+    click_link "Sign up now!"
+    page.should have_selector 'title', text: 'Sign up'
+    click_link "sample app"
+    page.should have_selector 'title', text: full_title('')
   end
 end
